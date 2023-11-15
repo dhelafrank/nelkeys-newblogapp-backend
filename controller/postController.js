fd802b8a63ce334832e5f5f4b37a43a4d222c7a7
dhelafrank <ubongabasijerome8@gmail.com>
2023-11-15 09:21:45 +0100

    Added Timestamps as Date

---
+++ controller/postController.js
@@ -0,0 +1,42 @@
+const fs = require("fs")
+const path = require("path")
+const { post } = require("..")
+const postDataPath = path.join(__dirname, "../data/post.json")
+
+
+const postClass = class posts {
+    allPosts() {
+        try {
+            const postsData = fs.readFileSync(postDataPath, "UTF-8")    
+            return JSON.parse(postsData)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    byID(id){
+        try {
+            const postsData = fs.readFileSync(postDataPath, "UTF-8")    
+            const allPosts = JSON.parse(postsData)
+            let foundPost = allPosts.find(post => post.id == id) || {"Error":`Post With ID ${id} doesn't exist`}
+            return foundPost
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    byBlob(blob){
+        try {
+            const postsData = fs.readFileSync(postDataPath, "UTF-8")    
+            const allPosts = JSON.parse(postsData)
+
+            const decodedBlob = blob.replace("-" && "_", " ")
+            let foundPost = allPosts.find(post => post.title == decodedBlob) || {"Error":`Post doesn't exist`}
+            return foundPost
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+module.exports = {postClass}
---
+++ routes/allPosts.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const router = express.Router()
+const postController = require("../controller/postController")
+
+const postControllerClass = new postController.postClass()
+
+router.get("/all", (req, res)=>{
+        try {
+            res.send(postControllerClass.allPosts())
+        } catch (error) {
+            console.log(error);
+        }
+})
+
+router.get('/id/:id', (req, res) => {
+    try {
+        res.send(postControllerClass.byID(req.params.id))
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/blob/:blob', (req, res) => {
+    try {
+        res.send(postControllerClass.byBlob(req.params.blob))
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+module.exports = router

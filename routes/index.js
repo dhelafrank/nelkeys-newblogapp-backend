d36d9fbbb839b79743269920c247fadbdd59a040
dhelafrank <ubongabasijerome8@gmail.com>
2023-11-16 01:16:36 +0100

    Removed View Engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'New Blog App' });
+  // res.render('index', { title: 'New Blog App' });
+  res.send("green")
 });
 
 module.exports = router;
